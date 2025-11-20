12b60b37758de2cdbc9c67d14e899e4ccb1025bb
Jennifer Jansson <jenniferjansson92@gmail.com>
2025-11-20 20:02:43 +0100

    connect Journey to posts.json and added more data

--- src/sections/Journey.jsx
+++ src/sections/Journey.jsx
@@ -1,8 +1,30 @@
 // src/sections/Journey.jsx
+import posts from "../data/posts.json";
+
 export const Journey = () => {
   return (
-    <section>
+    <section id="my-words">
       <h2>My words</h2>
+
+      {posts.map((post) => (          //loop through posts data and render each post
+        <article key={post.id}>      {/*helps React keep track of list items*/}
+          {post.image && (           //Only render image if it exists
+            <img src={post.image.src} alt={post.image.alt} />
+          )}
+
+          <p>{post.badge}</p>
+          <h3>{post.title}</h3>
+          <p>{post.excerpt}</p>
+
+          <a
+            href={post.link}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Read article
+          </a>
+        </article>
+      ))}
     </section>
   );
 };
