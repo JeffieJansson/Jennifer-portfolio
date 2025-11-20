a5f52d307b34da44b8c5829808e11da799f3666c
Jennifer Jansson <jenniferjansson92@gmail.com>
2025-11-20 20:19:11 +0100

    render all data from projects.json to Project file

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,28 @@
 export const Card = ({ project }) => {
   return (
     <article className="card">
+      {project.image && (
+        <img src={project.image.src} alt={project.image.alt} />
+      )}
+
       <h3>{project.title}</h3>
+
+      {project.tags && (     // Check if tags exist
+        <p>
+          {project.tags.join(" · ")} {/* join tags with dot separator for now */}
+        </p>
+      )}
+
       <p>{project.summary}</p>
+      <p>
+        <a href={project.live} target="_blank" rel="noreferrer">
+          Live demo
+        </a>{" "}
+        |{" "}
+        <a href={project.code} target="_blank" rel="noreferrer">
+          View code
+        </a>
+      </p>
     </article>
   );
 };
