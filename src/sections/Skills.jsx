110497be6f5c0bf1f7f5f51594e19b34e8b02840
Jennifer Jansson <jenniferjansson92@gmail.com>
2025-11-20 19:26:47 +0100

    Connect Skills section to skills.json data

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,8 +1,24 @@
-// src/sections/Journey.jsx
+// src/sections/Skills.jsx
+import skillsData from "../data/skills.json";
+
 export const Skills = () => {
+  const { code, toolbox, upcoming, more } = skillsData;
+
   return (
-    <section>
+    <section id="skills">
       <h2>Skills</h2>
+
+      <h3>Code</h3>
+      <p>{code.join(", ")}</p>
+
+      <h3>Toolbox</h3>
+      <p>{toolbox.join(", ")}</p>
+
+      <h3>Upcoming</h3>
+      <p>{upcoming.join(", ")}</p>
+
+      <h3>More</h3>
+      <p>{more.join(", ")}</p>
     </section>
   );
 };
