545f06d2e739beef11c38b3df8485793e89f8ad7
Jennifer Jansson <jenniferjansson92@gmail.com>
2025-11-20 19:06:16 +0100

    Connect Tech section to tech.json data

--- src/sections/Tech.jsx
+++ src/sections/Tech.jsx
@@ -1,8 +1,13 @@
 // src/sections/Journey.jsx
+import techData from "../data/tech.json";
+
 export const Tech = () => {
+  const techLine = techData.tech.join(", ");
+
   return (
-    <section>
+    <section id="tech">
       <h2>Tech</h2>
+      <p>{techLine}</p>
     </section>
   );
 };
