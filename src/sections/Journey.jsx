07f418c0f0d65505e36cf69f70a262b2345cb92b
Jennifer Jansson <jenniferjansson92@gmail.com>
2025-11-21 12:28:12 +0100

    redo structure so this file has code for structure and import styles from card/button/icons

--- src/sections/Journey.jsx
+++ src/sections/Journey.jsx
@@ -1,28 +1,41 @@
 // src/sections/Journey.jsx
 import posts from "../data/posts.json";
+import Button from "../components/Button";
+import { Tag } from "../components/Card";
+import { LiveIcon } from "../components/Icons";
 
 export const Journey = () => {
   return (
     <section id="my-words">
       <h2>My words</h2>
 
-      {posts.map((post) => (          //loop through posts data and render each post
-        <article key={post.id}>      {/*helps React keep track of list items*/}
-          {post.image && (           //Only render image if it exists
-            <img src={post.image.src} alt={post.image.alt} />
+      {posts.map((post) => ( // map over posts array
+        <article key={post.id}>   {/* unique key for each post*/}
+          {/* Thumb-img */}
+          {post.image && ( // render image if it exists in JSON
+            <img
+              src={post.image.src}
+              alt={post.image.alt}
+            />
           )}
 
-          <p>{post.badge}</p>
+          {/* Datum-badge som tagg */}
+          {post.badge && <Tag>{post.badge}</Tag>} {/* render badge if it exists in JSON*/}
+
           <h3>{post.title}</h3>
           <p>{post.excerpt}</p>
 
-          <a
-            href={post.link}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Read article
-          </a>
+          {/* Read article-button same style as LiveIcon button */}
+          {post.link && (               // render button if link exists in JSON
+            <Button
+              href={post.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LiveIcon aria-hidden="true" />
+              <span>Read article</span>
+            </Button>
+          )}
         </article>
       ))}
     </section>
