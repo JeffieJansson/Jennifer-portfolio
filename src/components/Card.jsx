5289b1073f33c7132d76a9a2ae517e23f439b567
Jennifer Jansson <jenniferjansson92@gmail.com>
2025-11-25 18:56:44 +0100

    modified styles for tagsrow

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,20 +2,20 @@ import styled from "styled-components";
 
 // (HTML, CSS, API…)
 export const TagsRow = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 0.5rem;
-  margin: 0.5rem 0;
+display: flex;
+align-items: flex-start;
+gap: 4px;
+align-self: stretch;
 `;
 
 // single tag
 export const Tag = styled.span`
-  display: flex;
-  width: 142px;
-  padding: 2px 6px;
-  justify-content: center;
-  align-items: flex-start;
-  border-radius: 4px;
-  border: 1px solid #000;
-  background: #FFF;
+display: flex;
+width: 142px;
+padding: 2px 6px;
+justify-content: center;
+align-items: flex-start;
+border-radius: 4px;
+border: 1px solid #000;
+background: #FFF;
 `;
